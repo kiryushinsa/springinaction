01583dfbe14047a1d3d4035542d6c289fc7fc7c9
kiryushinsa <kiryushin.s.a@gmail.com>
2022-12-21 22:27:45 +0300

    Start book. Introduction. First page.

---
+++ src/test/java/com/kiryushinsa/book/springinaction/HomePageTest.java
@@ -0,0 +1,32 @@
+package com.kiryushinsa.book.springinaction;
+
+import com.kiryushinsa.book.springinaction.controllers.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomePageTest {
+
+    private MockMvc mockMvc;
+
+    public HomePageTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+
+        mockMvc.perform(get("/")). andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome page. Homies")));
+    }
+}
