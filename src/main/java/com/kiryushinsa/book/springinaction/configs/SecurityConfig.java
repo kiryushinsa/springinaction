f29d5b54d6e063f384e63358b0d1085dfed91511
kiryushinsa <kiryushin.s.a@gmail.com>
2023-01-26 09:21:26 +0300

    Page 92. JDBC authentication.

--- src/main/java/com/kiryushinsa/book/springinaction/configs/SecurityConfig.java
+++ src/main/java/com/kiryushinsa/book/springinaction/configs/SecurityConfig.java
@@ -2,14 +2,21 @@ package com.kiryushinsa.book.springinaction.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 public class SecurityConfig {
@@ -29,19 +36,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService users() {
+    public UserDetailsService users(DataSource dataSource) {
         // The builder will ensure the passwords are encoded before saving in memory
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        UserDetails user = users
+        UserDetails user = User.builder()
                 .username("user")
-                .password("password")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
                 .roles("USER")
                 .build();
-        UserDetails admin = users
+
+        UserDetails admin = User.builder()
                 .username("admin")
-                .password("password")
+                .password("{bcrypt}$2a$12$PBc8uxJVqWOJz.Rw1.SQQ.h/dUzFFhvjr0T3ZAGVz.UWYzQmBgN56")
                 .roles("USER", "ADMIN")
                 .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 }
